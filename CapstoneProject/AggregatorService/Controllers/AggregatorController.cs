5b5f0f0cc6b0279edfc92dd2d5981935ca3bcdc6
Nick <nef48@psu.edu>
2021-05-29 20:48:41 -0400

    Initial creation of the Aggregator service

---
+++ CapstoneProject/AggregatorService/Controllers/AggregatorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AggregatorService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AggregatorController : ControllerBase
+    {
+        private readonly ILogger<AggregatorController> _logger;
+
+        public AggregatorController(ILogger<AggregatorController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ CapstoneProject/AggregatorService/DataAccess/Models/ResultsContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AggregatorService.DataAccess
+{
+    public class ResultsContext : DbContext
+    {
+        public ResultsContext(DbContextOptions<ResultsContext> options)
+            : base(options)
+        {
+        }
+    }
+}
